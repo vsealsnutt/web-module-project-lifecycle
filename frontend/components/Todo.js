0491669d12a42c160edc30a9ff0a95541f5cae1c
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-18 16:09:39 -0500

    moved todo component piece to Todo.js

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -4,9 +4,7 @@ export default class Todo extends React.Component {
   render() {
     return (
       <div 
-        onClick={this.props.handleToggle(this.props.todo.id)} 
-        key={this.props.todo.id}
-      >
+        onClick={this.props.handleToggle(this.props.todo.id)}>
         {this.props.todo.name}{this.props.todo.completed ? '- complete' : ''}
       </div>
     )
