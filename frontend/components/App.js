10c26c63e801d8c5c804bfa1b05dc7e59f743c9d
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-18 13:57:27 -0500

    created a basic form structure, started building out componentDidMount

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = 'http://localhost:9000/api/todos';
 
 export default class App extends React.Component {
+  state = {
+    todos: []
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    this.fetchAllTodos();
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          <div>Walk the dog</div>
+          <div>Learn React</div>
+        </div>
+
+        <form id='todoForm'>
+          <input type='text' placeholder='Type todo'></input>
+          <input type='submit'></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
