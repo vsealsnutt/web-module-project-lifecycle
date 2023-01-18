225422309c62657b31e4749372ee49bd3d875b08
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-18 16:08:12 -0500

    moved todo component piece to Todo.js

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    return (
+      <div 
+        onClick={this.props.handleToggle(this.props.todo.id)} 
+        key={this.props.todo.id}
+      >
+        {this.props.todo.name}{this.props.todo.completed ? '- complete' : ''}
+      </div>
+    )
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
 export default class TodoList extends React.Component {
   render() {
@@ -7,7 +8,13 @@ export default class TodoList extends React.Component {
           <h2>Todos:</h2>
           {
             this.props.todos.map(todo => {
-              return <div onClick={this.props.handleToggle(todo.id)} key={todo.id}>{todo.name}{todo.completed ? '- complete' : ''}</div>
+              return (
+                <Todo 
+                  key={todo.id}
+                  handleToggle={this.props.handleToggle}
+                  todo={todo}
+                />
+              )
             })
           }
         </div>
